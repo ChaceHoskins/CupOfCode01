896b4f5cb1412e5e36d55ba996b54c5b1566629b
ChaceHoskins <chace.hoskins@yahoo.com>
2018-01-10 18:28:38 +0000

    Lesson 1 of too many

---
+++ CupOfCode01/Lesson1.py
@@ -0,0 +1,21 @@
+print('since this string is in quotes and tied to the print function python knows to "print" it on the screen')
+x = 9
+x = 15
+print(x)
+print(x+2)
+print(x*2)
+print(x**2)
+print(x/2)
+print(x//2)
+
+x = 'two'
+y = 'three'
+ln1 = 'I will start the line here...'
+ln2 = 'and finish the line here'
+ln3 = ln1+ln2
+
+print(ln3)
+print(ln3+ln3)
+print("'this is as test")
+
+'''This is a series that I have found on youtube that I am going to try and follow to teach me python'''
