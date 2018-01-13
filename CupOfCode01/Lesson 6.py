40e75eccd13f595e9fb08ec144983cf717442907
ChaceHoskins <chace.hoskins@yahoo.com>
2018-01-13 23:08:20 +0000

    Lesson 6 of too many

---
+++ CupOfCode01/Lesson 6.py
@@ -0,0 +1,38 @@
+frame = '{} {} {} {} {} {}'
+print('\n')
+print(frame.format('this','will','print','in','the ','frame',))
+print('\n')
+print(frame.format(7,3,9,1,6,8))
+print('\n')
+print(frame.format(frame,frame,frame,frame,frame,frame))
+print(frame.format(
+    'this is another ',
+    'way that you can ',
+    'add data that can be plugged into ',
+    'your empty curly brackets ',
+    'you will see this formatting ',
+    'when we do some machine learning and AI! ',
+))
+
+print(
+    'test one',
+    'test two',
+    'test three',
+)
+print('''
+test one
+    test two
+    testt three
+    ''')
+shopping_list = '''
+time to make a shopping list:
+\t** some protien!
+\t* maybe get some brownies
+\t if i have room in the cart ill get some veggies\t\n***all done\n
+'''
+print(shopping_list)
+x,y= (3,5)
+print(x)
+print(y)
+
+
