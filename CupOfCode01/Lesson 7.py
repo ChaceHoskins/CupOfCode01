f286dc011ae785a5198ea5a81abc7e9c4843c491
ChaceHoskins <chace.hoskins@yahoo.com>
2018-01-13 23:27:14 +0000

    Lesson 7 of too many

---
+++ CupOfCode01/Lesson 7.py
@@ -0,0 +1,9 @@
+print('I would never as a woman her age.....but...?????')
+age = input('How old are you? ')
+print('What is you favorite number?')
+favNumber = input()
+print('tonight for dinner I would like some...')
+dinner = input()
+print('So this {} year old person '
+      'has a a favorite number of {} '
+      'and tonight for dinner they would like {}.'.format(age,favNumber,dinner))
