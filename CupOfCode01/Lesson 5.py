6d1e6c3c47136ae330620127fc4056d44317a0b6
ChaceHoskins <chace.hoskins@yahoo.com>
2018-01-13 21:20:08 +0000

    Lesson 5 of too many

---
+++ CupOfCode01/Lesson 5.py
@@ -0,0 +1,31 @@
+computer_languages = 256
+x = "there are {} types of computer languages".format(computer_languages)
+
+
+dada = '32 bit'
+dodo = '64 bit'
+
+k = "some have {} others have {}".format(dada, dodo)
+
+print(x+'\n')
+print(k)
+
+print('I have a {1} system but others have a {0}'.format(dada, dodo))
+
+print(dada + dodo)
+print(dada*10)
+
+print('what goes up but never comes down?')
+print('''Do you want a 'hint?''')
+print("."*10)
+
+part_1 = 'Y'
+part_2 = 'O'
+part_3 = 'U'
+part_4 = 'R'
+part_5 = 'A'
+part_6 = 'G'
+part_7 = 'E'
+
+print(part_1+part_2+part_3+part_4, end='\n')
+print(part_5+part_6+part_7)
