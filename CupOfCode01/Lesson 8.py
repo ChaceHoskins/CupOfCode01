7b068b827834bfaf0edb7714706f6de1930c6ec3
ChaceHoskins <chace.hoskins@yahoo.com>
2018-01-14 22:17:15 +0000

    Lesson 8 of too many

---
+++ CupOfCode01/Lesson 8.py
@@ -0,0 +1,29 @@
+condition = 1
+while condition < 10:
+    print(condition)
+    condition  +=1
+
+print('\n')
+
+list_1 = [5,8,2,33,87,999,1]
+for eachitem in list_1:
+    print(eachitem)
+
+print('\n')
+list_1 = [5,8,2,33,87,999,1]
+print(sorted(list_1))
+
+print('\n')
+for x in range (4,30):
+    print(x)
+
+print('\n')
+for x in range(4,100,20):
+    print(x)
+print('\n')
+
+for x in range(1,10):
+    if x%2 ==0:
+        print('EVEN')
+    else:
+        print('ODD')
